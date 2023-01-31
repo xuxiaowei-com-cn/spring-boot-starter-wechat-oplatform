79f8b22e7140782b56065083224d24bfd82a320b
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-01-31 09:21:31 +0800

    :bulb: 更新注释

--- src/main/java/org/springframework/security/oauth2/server/authorization/client/WeChatOplatformWebsiteTokenResponse.java
+++ src/main/java/org/springframework/security/oauth2/server/authorization/client/WeChatOplatformWebsiteTokenResponse.java
@@ -9,9 +9,9 @@ package org.springframework.security.oauth2.server.authorization.client;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -39,7 +39,7 @@ public class WeChatOplatformWebsiteTokenResponse implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * 网页授权接口调用凭证,注意：此access_token与基础支持的access_token不同
+	 * 授权Token
 	 */
 	@JsonProperty("access_token")
 	private String accessToken;
